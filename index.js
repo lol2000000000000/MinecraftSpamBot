51b1f981067dedca39bc326926312c3495cc9e77
THEshezzzee <61621678+lol2000000000000@users.noreply.github.com>
2021-07-09 21:20:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ if(config.altening == true){
                 time: false,
                 title: false,
                 physics: config.physics,
-                blocks: true
+                blocks: false
             }
         });
         bot.on('login', () => {
